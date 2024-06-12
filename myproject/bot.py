5050eea70a6c8b3ff14ee4f1cab628ebffb08670
Riabokin Ruslan <rriabokin@gmail.com>
2024-06-12 13:11:17 +0300

    Changed identification by .env

--- myproject/bot.py
+++ myproject/bot.py
@@ -1,24 +1,25 @@
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart
 import asyncio
+import logging
+from config_reader import config
 
-async def read_token() -> str:
-    with open("token.txt", "r") as file:
-        return file.read().strip()
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token=config.bot_token.get_secret_value())
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def send_welcome(message: types.Message):
+    await message.reply("Привет!\nЯ Эхо-бот от Skillbox!\nОтправь мне любое сообщение, а я тебе обязательно отвечу.")
+
+
+@dp.message()
+async def echo(message: types.Message):
+    await message.answer(message.text)
 
 async def main() -> None:
-    API_TOKEN = await read_token()
-    bot = Bot(token=API_TOKEN)
-    dp = Dispatcher()
-
-    @dp.message(CommandStart())
-    async def send_welcome(message: types.Message):
-        await message.reply("Привет!\nЯ Эхо-бот от Skillbox!\nОтправь мне любое сообщение, а я тебе обязательно отвечу.")
-
-    @dp.message()
-    async def echo(message: types.Message):
-        await message.answer(message.text)
-
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
