7e6a4639c2805ca00ecd65ffe2e36cae45f20d78
Riabokin Ruslan <rriabokin@gmail.com>
2024-06-13 15:07:01 +0300

    Created a folder training_code

---
+++ training_code/keyboard.py
@@ -0,0 +1,61 @@
+from aiogram import Bot, Dispatcher, types, F
+from aiogram.filters import CommandStart, Command
+import asyncio
+import logging
+import sys
+import os
+
+# Добавьте корневую директорию в sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from myproject.config_reader import config
+
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+bot = Bot(token=config.bot_token.get_secret_value())
+dp = Dispatcher()
+
+@dp.message(Command("пюрешка"))
+async def cmd_pureshka(message: types.Message):
+    """Обработчик команды /пюрешка. Отправляет клавиатуру с вариантами ответа."""
+    kb = [
+        [types.KeyboardButton(text="С пюрешкой")],
+        [types.KeyboardButton(text="Без пюрешки")]
+    ]
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=kb,
+        resize_keyboard=True,
+        input_field_placeholder="Выберите способ подачи"
+    )
+    await message.answer("Как подавать котлеты?", reply_markup=keyboard)
+
+@dp.message(F.text.lower() == "с пюрешкой")
+async def with_puree(message: types.Message):
+    """Обработчик сообщения 'с пюрешкой'. Отправляет ответ 'Отличный выбор!'."""
+    await message.reply("Отличный выбор!")
+
+@dp.message(F.text.lower() == "без пюрешки")
+async def without_puree(message: types.Message):
+    """Обработчик сообщения 'без пюрешки'. Отправляет ответ 'Так невкусно!'."""
+    await message.reply("Так невкусно! Лучше с пюрешкой")
+
+@dp.message(CommandStart())
+async def send_welcome(message: types.Message):
+    """Обработчик команды /start. Отправляет приветственное сообщение."""
+    await message.reply("Привет!\nЯ Эхо-бот от Skillbox!\nОтправь мне любое сообщение, а я тебе обязательно отвечу.")
+
+@dp.message()
+async def echo(message: types.Message):
+    """Обработчик всех остальных сообщений. Отправляет эхо-ответ с именем и ID пользователя."""
+    await message.answer(f'Привет, {message.from_user.first_name}, твой номер id: {message.from_user.id}')
+
+async def main() -> None:
+    """Главная функция для запуска бота."""
+    try:
+        await dp.start_polling(bot)
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
+    finally:
+        await bot.session.close()
+
+if __name__ == '__main__':
+    asyncio.run(main())
